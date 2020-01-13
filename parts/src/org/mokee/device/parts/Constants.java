4805a229ee178c5e3b587dc2d06d34e863fc279c
XiNGRZ <chenxingyu92@gmail.com>
2020-01-13 14:25:08 +0800

    m1926: parts: Toggling FOD base on screen state
    Change-Id: I9210bae2e837c857ef911925d3d50e40a2f63d47

---
+++ parts/src/org/mokee/device/parts/Constants.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2020 The MoKee Open Source Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+package org.mokee.device.parts;
+
+class Constants {
+
+    static final String FOD_ENABLE = "/dev/mokee.touch@1.0/fod";
+
+}
