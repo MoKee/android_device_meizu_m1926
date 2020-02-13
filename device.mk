9b090dd8262e574e93b7c0a1fef31cca0d0d369e
XiNGRZ <chenxingyu92@gmail.com>
2020-02-13 09:53:06 +0800

    m1926: Use charger from stock
    Change-Id: Ib1117d65d76bd16436395f266184f9b46325b162

--- device.mk
+++ device.mk
@@ -19,6 +19,10 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_COPY_FILES += \
     vendor/mokee/config/permissions/vendor.mokee.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/vendor.mokee.biometrics.fingerprint.inscreen.xml
 
+# Charger
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/charger,root/res/images/charger)
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     mokee.biometrics.fingerprint.inscreen@1.0-service.m1926
